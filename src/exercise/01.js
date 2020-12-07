f982e353b8c105f75337133e53c54c5c34c98ac6
thomaskisner <tjkisner@gmail.com>
2020-12-07 18:12:16 -0500

    hook localstorage solution, hook useEffect solution, lazy state initialization, hook useEffect dependencies solution, custom hook solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,31 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+
+//////////SOLUTION 1//////////////
+// function Greeting() {
+//   const [name, setName] = React.useState("")
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+/////////SOLUTION 2///////////////
+
+//Set default value so if initialName === undefined, input won't switch from uncontrolled to controlled with user input
+function Greeting({initialName = ""}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
+
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +35,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Hans Gruber"/>
 }
 
 export default App
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,49 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+////////EXTRA CREDIT 3 DECLARATION/////////////////////////////
+const useLocalStorageState = (key, defaultValue = '') => {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
+  )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  /////////SOLUTION 1////////////////
+
+  //const [name, setName] = React.useState(window.localStorage.getItem("name") || initialName)
+
+  ////////EXTRA CREDIT 1 AND 2/////////////////////////////
+  ////////EXTRA CREDIT 1 LAZY LOADING INITIAL STATE////////
+  ////////EXTRA CREDIT 2 useEffect DEPENDENCIES////////////
+
+  // const [name, setName] = React.useState(()=> {
+  //   const initialState = window.localStorage.getItem("name") || initialName )
+
+  // React.useEffect(()=> {
+  //   window.localStorage.setItem("name", name)
+  //     }, [name])
+
+  ////////EXTRA CREDIT 1 AND 2/////////////////////////////
+  ////////EXTRA CREDIT 1 LAZY LOADING INITIAL STATE////////
+  ////////EXTRA CREDIT 2 useEffect DEPENDENCIES////////////
+
+  // const [name, setName] = React.useState(
+  //   () => window.localStorage.getItem('name') || initialName,
+  // )
+
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
+
+  ////////EXTRA CREDIT 3 CUSTOM HOOK IMPLEMENTATION////////////
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +54,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
